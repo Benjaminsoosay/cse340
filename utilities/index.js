faf272c7d43007c27f54b7844d170aebedaf8858
Daniel Benjamin chinonso iwuchukwu <benjaminsoosayiwuchukwu@gmail.com>
2025-11-16 01:07:57 -0800

    Update index.js

--- utilities/index.js
+++ utilities/index.js
@@ -1,7 +1,6 @@
-// Utilities module
 const utilities = {};
 
-// Format price with commas and USD symbol (Requirement 3)
+// Format price with commas and USD symbol
 utilities.formatPrice = (price) => {
   return new Intl.NumberFormat('en-US', {
     style: 'currency',
@@ -10,35 +9,12 @@ utilities.formatPrice = (price) => {
   }).format(price);
 };
 
-// Format mileage with commas (Requirement 9)
+// Format mileage with commas
 utilities.formatMileage = (mileage) => {
   return new Intl.NumberFormat('en-US').format(mileage);
 };
 
-// Build inventory list HTML (Requirement 6 for list view)
-utilities.buildInventoryList = (vehicles) => {
-  let html = '<div class="vehicle-list">';
-  vehicles.forEach(vehicle => {
-    const formattedPrice = utilities.formatPrice(vehicle.price);
-    const formattedMileage = utilities.formatMileage(vehicle.mileage);
-
-    html += `
-      <div class="vehicle-card">
-        <a href="/inventory/vehicle/${vehicle.id}">
-          <img src="${vehicle.image}" alt="${vehicle.year} ${vehicle.make} ${vehicle.model}" class="vehicle-thumb">
-          <h2>${vehicle.year} ${vehicle.make} ${vehicle.model}</h2>
-        </a>
-        <p class="vehicle-price">${formattedPrice}</p>
-        <p class="vehicle-mileage">${formattedMileage} miles</p>
-        <p class="vehicle-color">Color: ${vehicle.color}</p>
-      </div>
-    `;
-  });
-  html += '</div>';
-  return html;
-};
-
-// Build vehicle detail HTML (Requirement 6 for detail view)
+// Build vehicle detail HTML
 utilities.buildVehicleDetail = (vehicleData) => {
   const formattedPrice = utilities.formatPrice(vehicleData.price);
   const formattedMileage = utilities.formatMileage(vehicleData.mileage);
